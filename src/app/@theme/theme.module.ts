78796f59fe74306ef0f0ba8e2acd1931e80e33a8
thanhdt <thanh.dt.95@gmail.com>
2018-07-27 18:42:06 +0700

    20180727 init struture

---
+++ src/app/@theme/theme.module.ts
@@ -0,0 +1,103 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import {
+  NbActionsModule,
+  NbCardModule,
+  NbLayoutModule,
+  NbMenuModule,
+  NbRouteTabsetModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbTabsetModule,
+  NbThemeModule,
+  NbUserModule,
+  NbCheckboxModule,
+  NbPopoverModule,
+  NbContextMenuModule,
+} from '@nebular/theme';
+
+import { NbSecurityModule } from '@nebular/security';
+
+import {
+  FooterComponent,
+  HeaderComponent,
+  SearchInputComponent,
+} from './components/index.component';
+import { CapitalizePipe, PluralPipe, RoundPipe, TimingPipe } from './pipes/index.pipes';
+import {
+  OneColumnLayoutComponent,
+  SampleLayoutComponent,
+  ThreeColumnsLayoutComponent,
+  TwoColumnsLayoutComponent,
+} from './layouts/index.layout';
+import { DEFAULT_THEME } from './styles/theme.default';
+import { COSMIC_THEME } from './styles/theme.cosmic';
+import { CORPORATE_THEME } from './styles/theme.corporate';
+
+const BASE_MODULES = [CommonModule, FormsModule, ReactiveFormsModule];
+
+const NB_MODULES = [
+  NbCardModule,
+  NbLayoutModule,
+  NbTabsetModule,
+  NbRouteTabsetModule,
+  NbMenuModule,
+  NbUserModule,
+  NbActionsModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbCheckboxModule,
+  NbPopoverModule,
+  NbContextMenuModule,
+  NgbModule,
+  NbSecurityModule, // *nbIsGranted directive
+];
+
+const COMPONENTS = [
+  HeaderComponent,
+  FooterComponent,
+  SearchInputComponent,
+  OneColumnLayoutComponent,
+  SampleLayoutComponent,
+  ThreeColumnsLayoutComponent,
+  TwoColumnsLayoutComponent,
+];
+
+const ENTRY_COMPONENTS = [
+];
+
+const PIPES = [
+  CapitalizePipe,
+  PluralPipe,
+  RoundPipe,
+  TimingPipe,
+];
+
+const NB_THEME_PROVIDERS = [
+  ...NbThemeModule.forRoot(
+    {
+      name: 'default',
+    },
+    [ DEFAULT_THEME, COSMIC_THEME, CORPORATE_THEME ],
+  ).providers,
+  ...NbSidebarModule.forRoot().providers,
+  ...NbMenuModule.forRoot().providers,
+];
+
+@NgModule({
+  imports: [...BASE_MODULES, ...NB_MODULES],
+  exports: [...BASE_MODULES, ...NB_MODULES, ...COMPONENTS, ...PIPES],
+  declarations: [...COMPONENTS, ...PIPES],
+  entryComponents: [...ENTRY_COMPONENTS],
+})
+export class ThemeModule {
+  static forRoot(): ModuleWithProviders {
+    return <ModuleWithProviders>{
+      ngModule: ThemeModule,
+      providers: [...NB_THEME_PROVIDERS],
+    };
+  }
+}
