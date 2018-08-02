ffe174f89a6fe5be75004402989449188730057f
LeThanh <tubean.81@gmail.com>
2018-08-02 14:40:17 +0700

    commit

--- src/app/@core/services/authen.service.ts
+++ src/app/@core/services/authen.service.ts
@@ -4,29 +4,29 @@ import {HttpService} from './http.service';
 import {Observable} from 'rxjs/Observable';
 import {User} from '../models/user.model';
 import {BaseConfig} from '../glossary/BaseConfig';
+import {CookieService} from 'ngx-cookie-service';
 
 @Injectable()
 export class AuthenService {
 
-  constructor(private http: HttpClient, private httpService: HttpService) {
+  constructor(private http: HttpClient, private httpService: HttpService, private cookieService: CookieService) {
   }
 
   attemptAuth(user: User): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({}),
-      observe: 'response',
-    };
     const userJson = JSON.stringify({'username': user.username, 'password': user.password});
     return this.http.post(
       BaseConfig.protocol.toString() + BaseConfig.server.toString()
-      + BaseConfig.standardServicePort.toString() + '/api/login', userJson, httpOptions);
+      + BaseConfig.standardServicePort.toString() + '/api/login', userJson, {
+        headers: new HttpHeaders({'Content-Type': 'application/json'}),
+        observe: 'response',
+      });
 
   }
 
   login(user: User) {
     this.attemptAuth(user).subscribe(
       (res) => {
-        window.localStorage['Authorization'] = res.headers.get('Authorization');
+        this.cookieService.set('Authorization', res.headers.get('Authorization'));
         return true;
       },
       (error) => {
