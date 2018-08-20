c269f65f5e7455e24c3523eed12a3903ef731bc3
ledevthang <leducthangdev2912@gmail.com>
2018-08-20 08:53:01 +0700

    change api
    change api http://192.168.1.137/kpi/api/users

--- src/app/@core/services/management-users.service.ts
+++ src/app/@core/services/management-users.service.ts
@@ -1,10 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Rx';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ManagementUsersService {
+export class ManagementUsersService implements OnInit {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      // 'Authorization':
+    }),
+  };
 
-  constructor() { }
+  constructor( private http: HttpClient) {
+  }
+  // url = '../../../assets/champions.json';
+  url = 'http://192.168.1.137:8080/kpi/api/users';
 
+  ngOnInit() {
+  }
+  getUser(): Observable<any> {
+    return this.http.get(this.url, this.httpOptions);
+  }
 }
--- src/app/pages/acc-management/acc-management.component.ts
+++ src/app/pages/acc-management/acc-management.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {BsModalRef, BsModalService} from 'ngx-bootstrap';
 import {AskSaveComponent} from './ask-save/ask-save.component';
 import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import {ManagementUsersService} from '../../@core/services/management-users.service';
 
 @Component({
   selector: 'acc-management',
@@ -9,29 +10,39 @@ import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./acc-management.component.scss'],
 })
 export class AccManagementComponent implements OnInit {
+  list = [];
 
   constructor(
     private bsModal: NgbModal,
+    private mService: ManagementUsersService
   ) { }
   select = 'Man';
   show = true;
   logoutModal: NgbModalRef;
   ngOnInit() {
+    this.mService.getUser().subscribe(data => {
+      this.list = data;
+    })
   }
   myFunction() {
-    let input, filter, managementContent, div, p, i;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    managementContent = document.getElementById('managementContent');
-    div = managementContent.getElementsByTagName('div');
-    for (i = 0; i < div.length; i++) {
-      p = div[i].getElementsByTagName('p')[0];
-      if (p.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        div[i].style.display = '';
-      } else {
-        div[i].style.display = 'none';
+      // Declare variables
+      let input, filter, table, tr, td, i;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
+
+      // Loop through all table rows, and hide those who don't match the search query
+      for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+          if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
       }
-    }
   }
   edit() {
     this.show = !this.show;
