3b96b973c39203242f952f225299cac2ccf27ad6
Lengo <lengocnghiemduong@gmail.com>
2018-08-22 22:52:46 +0700

    - Create club service - Create + Update Club Activities

---
+++ src/app/@core/services/club.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+import {HttpClient} from '@angular/common/http';
+import {HttpService} from './http.service';
+import {BaseConstant} from '../glossary/base.constant';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClubService {
+
+  constructor(private http: HttpClient, private httpService: HttpService) {
+  }
+
+  private clubUrl = BaseConstant.protocol.toString() + BaseConstant.server.toString()
+    + BaseConstant.standardServicePort.toString() + '/api/clubs';
+
+  private httpOptions = this.httpService.setHeaderToken();
+
+  addClub(club): Observable<any> {
+
+    return this.http.post(this.clubUrl, club, this.httpOptions);
+  }
+
+  updateClub(id, club): Observable<any> {
+    return this.http.put(`${this.clubUrl}/${id}`, club, this.httpOptions);
+  }
+}
