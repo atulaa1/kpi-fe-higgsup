41311fd848e460d45929c9313ff539df9ef7e2e5
ducnh <dukeph03nix@gmail.com>
2018-08-22 18:06:09 +0700

    Add model and DTO of [project] object Add code calling API Add, Edit, Delete project

---
+++ src/app/@core/services/project.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpService} from './http.service';
+import {BaseConstant} from '../glossary/base.constant';
+import {Project} from '../models/project.model';
+import {Observable} from '../../../../node_modules/rxjs/Rx';
+import {ResponseProjectDTO} from '../models/response-project-dto.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectService {
+
+  constructor(private http: HttpClient,
+              private httpService: HttpService) {
+  }
+
+  private url = BaseConstant.protocol.toString() + BaseConstant.server.toString() + BaseConstant.standardServicePort.toString();
+  private httpOptions = this.httpService.setHeaderToken();
+
+  getAllProject(): Observable<ResponseProjectDTO> {
+    return this.http.get<ResponseProjectDTO>(this.url + '/api/projects', this.httpOptions);
+  }
+
+  addNewProject(project: Project): Observable<ResponseProjectDTO> {
+    let projectJson = JSON.stringify(project);
+    return this.http.post<ResponseProjectDTO>(this.url + '/api/projects', projectJson, this.httpOptions);
+  }
+}
--- src/app/pages/projectmanagement/projectmanagement.component.ts
+++ src/app/pages/projectmanagement/projectmanagement.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Project} from '../../@core/models/project.model';
+import {ProjectService} from '../../@core/services/project.service';
+import {ResponseProjectDTO} from '../../@core/models/response-project-dto.model';
 
 @Component({
   selector: 'projectmanagement',
@@ -9,11 +12,15 @@ export class ProjectmanagementComponent implements OnInit {
   active: boolean = true;
   add: boolean = true;
   event;
+  projects: Project[];
+  newProject = new Project();
 
-  constructor() {
+  constructor(private projectService: ProjectService) {
   }
 
   ngOnInit() {
+    this.getListProject();
+    // this.confirmAdd(event);
   }
 
   Active() {
@@ -31,7 +38,15 @@ export class ProjectmanagementComponent implements OnInit {
   confirmAdd(event) {
     const code = (event.keyCode ? event.keyCode : event.which);
     if (event.keyCode === 13) {
+      this.projectService.addNewProject(this.newProject);
+      this.getListProject();
       this.add = !this.add;
     }
+  }
+
+  getListProject() {
+    this.projectService.getAllProject().subscribe((response: ResponseProjectDTO) => {
+      this.projects = response.data;
+    });
   }
 }
