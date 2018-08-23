050b6190e8884f910253ed3d29c6009b58bb3537
Lengo <lengocnghiemduong@gmail.com>
2018-08-23 17:57:33 +0700

    - Creat + Update Seminar Activity

---
+++ src/app/@core/glossary/base.constant.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var BaseConstant = /** @class */ (function () {
+    function BaseConstant() {
+    }
+    BaseConstant.protocol = 'http://';
+    BaseConstant.server = '192.168.1.137';
+    BaseConstant.standardServicePort = ':8080/kpi';
+    return BaseConstant;
+}());
+exports.BaseConstant = BaseConstant;
--- src/app/pages/acti-management/create-acti/seminar/seminar.component.ts
+++ src/app/pages/acti-management/create-acti/seminar/seminar.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NgbActiveModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {CreatedActivity} from '../../../../@core/models/createdActivity.model';
+import {Group} from '../../../../@core/models/group.model';
+import {SeminarService} from '../../../../@core/services/seminar.service';
+import {Activity} from '../../../../@core/models/activity.model';
 
 @Component({
   selector: 'ngx-seminar',
@@ -12,8 +15,52 @@ export class SeminarComponent implements OnInit {
   @Input() createdActivity: CreatedActivity = new CreatedActivity();
   @Input() activityName: string = '';
   @Input() dismiss;
-  constructor(private activeModal: NgbActiveModal) {
+  @Input() groupId: number = null;
+  seminarActivity = new Group<CreatedActivity>();
+
+  constructor(private activeModal: NgbActiveModal, private seminarService: SeminarService) {
   }
+
+
+
+  onAddSeminar() {
+    const groupType = new Activity();
+    groupType.id = 1;
+    this.seminarActivity.groupTypeId = groupType;
+    this.seminarActivity.additionalConfig = this.createdActivity;
+    this.seminarActivity.name = this.activityName;
+    return this.seminarService.addSeminar(this.seminarActivity).subscribe(response => {
+      if (response.status_code === 200) {
+        this.activeModal.close();
+        swal('Chúc Mừng!', 'Đã tạo thành công!', 'success');
+      }
+      else if (response.status_code === 940) {
+        swal('Thông báo!', 'Điểm của người phía dưới không được lớn hơn!', 'error');
+      }
+      else if (response.status_code === 901) {
+        swal('Thông báo!', 'Điểm của Host không hợp lệ!', 'error');
+      }
+
+    })
+  }
+
+  onUpdateSeminar() {
+    const groupType = new Activity();
+    groupType.id = 1;
+    this.seminarActivity.groupTypeId = groupType;
+    this.seminarActivity.additionalConfig = this.createdActivity;
+    this.seminarActivity.name = this.activityName;
+    return this.seminarService.updateSeminar(this.groupId, this.seminarActivity).subscribe(response => {
+      if (response.status_code === 200) {
+        this.dismiss();
+        return swal('Chúc Mừng!', 'Đã sửa thành công!', 'success');
+      }
+      else if (response.status_code === 906) {
+        return swal('Thông báo!', 'Hoạt động này đã tồn tại!', 'error');
+      }
+    })
+  }
+
 
   ngOnInit() {
   }
