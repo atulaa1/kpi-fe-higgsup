812027908f525f78f9e2a30bf8d1b41eb3f69a37
AtulaPham <phamxuandunga@gmail.com>
2018-08-24 18:32:44 +0700

    import NgbActiveModel at appMoDule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {CoreModule} from './@core/core.module';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {ThemeModule} from './@theme/theme.module';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbActiveModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {BsModalRef, BsModalService, ModalModule} from 'ngx-bootstrap';
 import { LoginComponent } from './modals/login/login.component';
 import { LogoutComponent } from './modals/logout/logout.component';
@@ -34,7 +34,7 @@ import {AskSaveComponent} from './pages/acc-management/ask-save/ask-save.compone
     ModalModule.forRoot(),
   ],
   bootstrap: [AppComponent],
-  providers: [BsModalService, BsModalRef,
+  providers: [BsModalService, BsModalRef, NgbActiveModal,
     {provide: APP_BASE_HREF, useValue: '/'},
   ],
   entryComponents: [LoginComponent, PersonalInfoComponent, LogoutComponent],
