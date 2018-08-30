48d0f62f3ce302e9b3c319f98a1540e3d9c9e615
tungdao <robinsaurang@gmail.com>
2018-08-30 13:11:23 +0700

    - survey :    + edit background button    + change rating star    + update handle error (900)    + fix bug logic modal (quan ly khao sat)

--- src/app/pages/list-of-survey/list-of-survey.module.ts
+++ src/app/pages/list-of-survey/list-of-survey.module.ts
@@ -4,11 +4,13 @@ import {ListOfSurveyComponent} from './list-of-survey.component';
 import { PersonalSurveyComponent } from './modal-kscn/modal-kscn.component';
 import { SeminarSurveyComponent } from './kssml/kssml.component';
 import {FormsModule} from '@angular/forms';
+import {ThemeModule} from "../../@theme/theme.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ThemeModule,
   ],
   declarations: [ListOfSurveyComponent, PersonalSurveyComponent, SeminarSurveyComponent ],
   entryComponents: [PersonalSurveyComponent, SeminarSurveyComponent],
