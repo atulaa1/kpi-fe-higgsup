17aa2bfb9fe5cbd23f93d8b9ff1d2f27bb6188eb
Lengo <lengocnghiemduong@gmail.com>
2018-08-31 10:22:07 +0700

    Fix bug + Improve UI Modal - Fix bug SPC-01 and SPC-09 - Add alert message for Modal: Teambuilding, Club, Seminar

--- src/app/pages/acti-management/create-acti/team-building/team-building.component.ts
+++ src/app/pages/acti-management/create-acti/team-building/team-building.component.ts
@@ -19,7 +19,9 @@ export class TeamBuildingComponent implements OnInit {
   @Input() dismiss;
   teambuilding = new Group<CreatedActivity>();
   message: string;
+  alert: boolean = false;
   @Output() change = new EventEmitter<any>();
+
   constructor(private activeModal: NgbActiveModal, private teambuildingService: TeambuildingService,
               private data: DataService) {
   }
@@ -32,52 +34,70 @@ export class TeamBuildingComponent implements OnInit {
   onAddTeambuilding(addNew: any) {
     let point = new CreatedActivity();
     const groupType = new Activity();
-    point  = this.createdActivity;
+    point = this.createdActivity;
     groupType.id = 3;
     this.teambuilding.groupTypeId = groupType;
     this.teambuilding.additionalConfig = point;
     this.teambuilding.name = this.activityName;
-    return this.teambuildingService.addTeambuilding(this.teambuilding).subscribe(response => {
-      if (response.status_code === 200) {
-        this.activeModal.close();
-        this.data.changeMessage('Created new an activity');
-        swal('Chúc Mừng!', 'Đã tạo thành công!', 'success');
-      } else if (response.status_code === 906) {
-        swal('Thông báo!', 'Hoạt động này đã tồn tại!', 'error');
-      } else if (response.status_code === 900) {
-        swal('Thông báo!', 'Không tìm thấy loại hoạt động!', 'error');
-      } else if (response.status_code === 932) {
-        swal('Thông báo!', 'Hoạt động này đã tồn tại!', 'error');
-      } else if (response.status_code === 931) {
-        swal('Thông báo!', 'Điểm của giải Nhất không được để trống!', 'error');
-      }
-    });
+    if (this.teambuilding.name === ''
+      || this.teambuilding.additionalConfig.firstPrize === null
+      || this.teambuilding.additionalConfig.secondPrize === null
+      || this.teambuilding.additionalConfig.thirdPrize === null
+      || this.teambuilding.additionalConfig.organizers === null) {
+      this.alert = true;
+    } else {
+      this.alert = false;
+      return this.teambuildingService.addTeambuilding(this.teambuilding).subscribe(response => {
+        if (response.status_code === 200) {
+          this.activeModal.close();
+          this.data.changeMessage('Created new an activity');
+          swal('Chúc Mừng!', 'Đã tạo thành công!', 'success');
+        } else if (response.status_code === 906) {
+          swal('Thông báo!', 'Hoạt động này đã tồn tại!', 'error');
+        } else if (response.status_code === 900) {
+          swal('Thông báo!', 'Không tìm thấy loại hoạt động!', 'error');
+        } else if (response.status_code === 932) {
+          swal('Thông báo!', 'Hoạt động này đã tồn tại!', 'error');
+        } else if (response.status_code === 931) {
+          swal('Thông báo!', 'Điểm của giải Nhất không được để trống!', 'error');
+        }
+      });
+    }
   }
 
   onUpdateTeambuilding(update: any) {
     let point = new CreatedActivity();
     const groupType = new Activity();
-    point  = this.createdActivity;
+    point = this.createdActivity;
     groupType.id = 3;
     this.teambuilding.groupTypeId = groupType;
     this.teambuilding.additionalConfig = point;
     this.teambuilding.name = this.activityName;
     this.teambuilding.id = this.groupId;
-    return this.teambuildingService.updateTeambuilding(this.teambuilding).subscribe(response => {
-      if (response.status_code === 200) {
-        this.change.emit(update);
-        this.dismiss();
-        swal('Chúc Mừng!', 'Đã sửa thành công!', 'success');
-      } else if (response.status_code === 906) {
-        swal('Thông báo!', 'Hoạt động này đã tồn tại!', 'error');
-      } else if (response.status_code === 900) {
-        swal('Thông báo!', 'Không tìm thấy loại hoạt động!', 'error');
-      } else if (response.status_code === 932) {
-        swal('Thông báo!', 'Hoạt động này đã tồn tại!', 'error');
-      } else if (response.status_code === 931) {
-        swal('Thông báo!', 'Điểm của giải Nhất không được để trống!', 'error');
-      }
-    })
+    if (this.teambuilding.name === ''
+      || this.teambuilding.additionalConfig.firstPrize.toString() === ''
+      || this.teambuilding.additionalConfig.secondPrize.toString() === ''
+      || this.teambuilding.additionalConfig.thirdPrize.toString() === ''
+      || this.teambuilding.additionalConfig.organizers.toString() === '') {
+      this.alert = true;
+    } else {
+      this.alert = false;
+      return this.teambuildingService.updateTeambuilding(this.teambuilding).subscribe(response => {
+        if (response.status_code === 200) {
+          this.change.emit(update);
+          this.dismiss();
+          swal('Chúc Mừng!', 'Đã sửa thành công!', 'success');
+        } else if (response.status_code === 906) {
+          swal('Thông báo!', 'Hoạt động này đã tồn tại!', 'error');
+        } else if (response.status_code === 900) {
+          swal('Thông báo!', 'Không tìm thấy loại hoạt động!', 'error');
+        } else if (response.status_code === 932) {
+          swal('Thông báo!', 'Hoạt động này đã tồn tại!', 'error');
+        } else if (response.status_code === 931) {
+          swal('Thông báo!', 'Điểm của giải Nhất không được để trống!', 'error');
+        }
+      })
+    }
   }
 
   ngOnInit() {
