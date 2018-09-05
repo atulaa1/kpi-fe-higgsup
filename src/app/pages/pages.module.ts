ee4e92d4c229c36903a785447bd0e1a9b10ee36d
AtulaPham <phamxuandunga@gmail.com>
2018-09-05 10:47:28 +0700

    remove used module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,10 +7,8 @@ import { ThemeModule } from '../@theme/theme.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 import {ActiManagementModule} from './acti-management/acti-management.module';
 import {ProjectmanagementComponent} from './projectmanagement/projectmanagement.component';
-import {AccManagementModule} from './acc-managements/acc-management/acc-management.module';
 import {ListOfSurveyModule} from './list-of-survey/list-of-survey.module';
 import {DialogConfirmationComponent} from '../modals/dialog-confirmation/dialog-confirmation.component';
-import { AccManagementsComponent } from './acc-managements/acc-managements.component';
 import {AccManagementsModule} from './acc-managements/acc-managements.module';
 
 const PAGES_COMPONENTS = [
