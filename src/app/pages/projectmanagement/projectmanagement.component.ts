f4e7c08fb6a824251eebbc4aecf6367086f0c7f0
Lengo <lengocnghiemduong@gmail.com>
2018-09-12 13:16:15 +0700

    - Hiển thị fullname thay vì last và first name - Edit table project management

--- src/app/pages/projectmanagement/projectmanagement.component.ts
+++ src/app/pages/projectmanagement/projectmanagement.component.ts
@@ -35,28 +35,6 @@ export class ProjectmanagementComponent implements OnInit {
     this.getListProject();
   }
 
-  // active(project: Project, content) {
-  //   if (this.isAdding === false && this.isEditing === false) {
-  //     this.actionType = 'EDIT';
-  //     this.msg = 'Bạn có muốn kích hoạt dự án ' + project.name + ' không';
-  //     this.buttonTitle = 'Lưu';
-  //     this.currentProject = Object.assign({}, project);
-  //     this.currentProject.active = 1;
-  //     this.bsModal.open(content, {backdrop: 'static', centered: true});
-  //   }
-  // }
-  //
-  // deactive(project: Project, content) {
-  //   if (this.isAdding === false && this.isEditing === false) {
-  //     this.actionType = 'EDIT';
-  //     this.msg = 'Bạn có muốn dừng hoạt động của dự án ' + project.name + ' không?';
-  //     this.buttonTitle = 'Lưu';
-  //     this.currentProject = Object.assign({}, project);
-  //     this.currentProject.active = 0;
-  //     this.bsModal.open(content, {backdrop: 'static', centered: true});
-  //   }
-  // }
-
   changeStatus(project: Project, content) {
     if (this.isAdding === false && this.isEditing === false) {
       const running = 'kích hoạt';
