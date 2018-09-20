468c1efb9cf9491fa9f6c7427b16bebb659e7ceb
ScarDL <lengocnghiemduong@gmail.com>
2018-09-20 10:30:08 +0700

    Set value default for date and Time

--- src/app/pages/activities/club-activity/club-activity.component.ts
+++ src/app/pages/activities/club-activity/club-activity.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit, ElementRef, ViewChild} from '@angular/core';
-import {NgbDateParserFormatter, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {NgbDateParserFormatter, NgbDateStruct, NgbTimeStruct} from '@ng-bootstrap/ng-bootstrap';
 import {KpiDateFormatter} from '../../../modals/personal-info/kpi-date-formatter';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
@@ -16,8 +16,11 @@ import {map, startWith} from 'rxjs/operators';
 })
 export class ClubActivityComponent implements OnInit {
   @Input() dismiss;
-  time = {hour: 12, minute: 0o0};
+  startTime = {hour: 12, minute: 0o0, second: 0o0};
+  endTime = {hour: 12, minute: 0o0, second: 0o0};
   spinners: boolean = false;
+  startDate: NgbDateStruct;
+  endDate: NgbDateStruct;
 
   visible = true;
   selectable = true;
@@ -75,8 +78,33 @@ export class ClubActivityComponent implements OnInit {
     return this.allFruits.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
   }
 
+  convertDatetoNgbDateStruct(date: Date): NgbDateStruct {
+    return date ? {year: date.getFullYear(), month: date.getMonth() + 1, day: date.getDate()} : null;
+  }
+
+  private convertNgbDateStructToString(date: NgbDateStruct): string {
+    return date ? `${this.padNumber(date.day)}-${this.padNumber(date.month)}-${date.year}` : null;
+  }
+
+  private convertNgbtimeStructToString(time: NgbTimeStruct): string {
+    return time ? `${this.padNumber(time.hour)}:${this.padNumber(time.minute)}` : null;
+  }
+
+  private isNumber(value: any): boolean {
+    return !isNaN(parseInt(value, 10));
+  }
+
+  private padNumber(value: number) {
+    if (this.isNumber(value)) {
+      return `0${value}`.slice(-2);
+    } else {
+      return '';
+    }
+  }
+
   ngOnInit() {
   }
+
   closeModal() {
     this.dismiss();
   }
