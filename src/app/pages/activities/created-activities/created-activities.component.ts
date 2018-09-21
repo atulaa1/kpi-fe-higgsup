c43ed4f0cdf99d3d0d684de842cb13583daa7efd
ScarDL <lengocnghiemduong@gmail.com>
2018-09-21 15:22:23 +0700

    API Get Created Events

--- src/app/pages/activities/created-activities/created-activities.component.ts
+++ src/app/pages/activities/created-activities/created-activities.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivitiesService} from '../../../@core/services/activities.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {Event} from '../../../@core/models/event.model';
 
 @Component({
   selector: 'ngx-created-activities',
@@ -8,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 export class CreatedActivitiesComponent implements OnInit {
   showMsg: boolean = false;
   nameSearch: string;
-  constructor() { }
+  groupList: Array<Event>;
+
+  constructor(private activitiesService: ActivitiesService, private modalService: NgbModal) {
+  }
 
   ngOnInit() {
+    this.activitiesService.getCreatedActivitiesByMem().subscribe(response => {
+      if (response.status_code === 200) {
+        this.groupList = response.data;
+        this.sortActivityArrayByType();
+      }
+    });
   }
+
   handleKeyDown(event: any) {
     if (event.keyCode === 13) {
       this.mySearchFunction();
@@ -38,5 +51,25 @@ export class CreatedActivitiesComponent implements OnInit {
         }
       }
     }
+  }
+
+  open(content) {
+    this.modalService.open(content, {backdrop: 'static', centered: true, size: 'lg'});
+  }
+
+  sortActivityArrayByType() {
+    this.groupList.sort((a, b) => {
+      const nameA = a.group.groupType.name.toUpperCase(); // bỏ qua in hoa thường
+      const nameB = b.group.groupType.name.toUpperCase(); // bỏ qua in hoa thường
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    });
   }
 }
