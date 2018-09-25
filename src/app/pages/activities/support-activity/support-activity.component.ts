bc4f769d657f6179ae2ad617bc32035d1d453da1
tung <robinsaurang@gmail.com>
2018-09-25 17:28:07 +0700

    cheange name function (searchUserInfo)

--- src/app/pages/activities/support-activity/support-activity.component.ts
+++ src/app/pages/activities/support-activity/support-activity.component.ts
@@ -1,6 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
 import {KpiDateFormatter} from '../../../modals/personal-info/kpi-date-formatter';
+import {SupportService} from '../../../@core/services/support.service';
+import {Event} from '../../../@core/models/event.model';
+import {EventAdditionalConfig} from '../../../@core/models/eventAdditionalConfig.model';
+
 
 @Component({
   selector: 'ngx-support-activity',
@@ -11,8 +15,16 @@ import {KpiDateFormatter} from '../../../modals/personal-info/kpi-date-formatter
 export class SupportActivityComponent implements OnInit {
   @Input() dismiss;
   showInput: boolean = false;
-
-  constructor() {
+  supportNumber: number = 0;
+  supportEvent: Event<EventAdditionalConfig>;
+  supportActivity = {
+    clearUp : null,
+    shopping : null,
+    supportSeminor : null,
+    cleaningWeek : null,
+    training : null,
+  }
+  constructor(private supportService: SupportService) {
   }
 
   ngOnInit() {
@@ -26,8 +38,15 @@ export class SupportActivityComponent implements OnInit {
 
   }
 
+  addSupport() {
+    this.supportService.createEventSupport(this.supportEvent).subscribe(response => {
+      this.supportEvent = response.data;
+    })
+  }
+
   openInputNumber() {
     this.showInput = !this.showInput;
+    this.supportNumber = 1;
   }
 
 }
