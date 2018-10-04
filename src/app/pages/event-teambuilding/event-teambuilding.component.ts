851819b27f1ca8f0329c4943e997c9fe40ed3a08
tung <robinsaurang@gmail.com>
2018-10-04 14:46:23 +0700

    confirm activities + add title and style select option

---
+++ src/app/pages/event-teambuilding/event-teambuilding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'event-teambuilding',
+  templateUrl: './event-teambuilding.component.html',
+  styleUrls: ['./event-teambuilding.component.scss']
+})
+export class EventTeambuildingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
